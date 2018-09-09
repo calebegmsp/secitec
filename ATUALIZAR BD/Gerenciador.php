8249a2432317cd4e02bfa95e59257795ca7d5e06
calebegmsp <calebegpiloes@gmail.com>
2018-09-09 19:14:01 -0300

    Alterado sistema de gerenciamento dos dados

--- ATUALIZAR BD/Gerenciador.php
+++ ATUALIZAR BD/Gerenciador.php
@@ -92,7 +92,7 @@
 
                                         </td>
                                         <td class="text-right">
-                                            <button id="<?= $minicursos[$key]['id_Mcurso']; ?>" type="button" class="btn btn-primary btn-editar" data-toggle="modal" data-target="#exampleModalCenter">
+                                            <button id="<?= $minicursos[$key]['id_Mcurso']; ?>" type="button" class="btn btn-primary btn-editar" data-toggle="modal" data-target="#mini<?= $minicursos[$key]['id_Mcurso']; ?>">
                                               Editar
                                             </button> 
                                         </td>
@@ -102,7 +102,123 @@
                         </div>
                     </div>
                 </div>
-            </div>            
+            </div> 
+
+
+<!-- Modal -->
+<div class="modal fade" data-backdrop="static" id="mini<?= $minicursos[$key]['id_Mcurso']; ?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalCenterTitle">Alterar dados</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+            
+
+        <form action="Gerenciador.php" method="get">
+
+            <!--  Escolha do Curso -->
+            <label for="exampleInputEmail1">Curso</label>
+            <select class="form-control" name="id_Mcurso" id="id_Mcurso">
+
+                <?php
+
+                    $cursos = $sql->select("SELECT id_curso, nome_curso, imgPadrao_curso FROM Curso");
+                    foreach ($cursos as $key => $value) {
+                        if ($cursos[$key]['id_curso'] ==  $minicursos[$key]['FK_Curso_id_curso']) {
+                            echo "<option value =\"".$cursos[$key]['id_curso']. "\" selected >"  . $cursos[$key]['nome_curso']. "</option>";
+                        } else {
+                            echo "<option value =\"".$cursos[$key]['id_curso']. "\">" . $cursos[$key]['nome_curso']. "</option>";
+                        }
+                    }
+                ?>
+
+            </select>
+
+
+
+            <!-- Nome -->
+            <div class="form-group">
+                <label for="formGroupExampleInput">Nome</label>
+                <input type="text" class="form-control" id="nome_Mcurso" name="nome_Mcurso" value="<?=$minicursos[$key]['nome_Mcurso']; ?>" placeholder="">
+            </div>
+
+            <!-- Descrição -->
+            <div class="form-group">
+                <label for="exampleFormControlTextarea1">Descrição</label>
+                <textarea class="form-control" id="des_Mcurso"  name="des_Mcurso" rows="2"><?= $minicursos[$key]['des_Mcurso'];?></textarea>
+            </div>
+
+            <!-- Local -->
+            <div class="form-group">
+                <label for="formGroupExampleInput">Local</label>
+                <input type="text" class="form-control" id="local_Mcurso" name="local_Mcurso" placeholder="">
+            </div>
+
+
+            <!-- Imagen -->
+            <div class="form-group">
+                <label for="exampleFormControlFile1">Imagem</label>
+                <input type="file" class="form-control-file" id="img_Minicurso" name="img_Minicurso">
+            </div>
+
+
+            <div class="row">
+                <div class="col-md-6">
+                    <!--  Escolha do dia -->
+                    <label for="exampleInputEmail1" id="dia_Mcurso" name="dia_Mcurso" >Dia</label>
+                    <select class="form-control">
+                        <option value="16">16</option>
+                        <option value="17">17</option>
+                        <option value="18">18</option>
+                        <option value="19">19</option>
+                    </select>                    
+                </div>
+
+                 <div class="col-md-6">
+                    <label for="formGroupExampleInput">Hora</label>
+                    <input type="text" class="form-control" id="hora_Mcurso" name="hora_Mcurso" placeholder="">
+                </div>               
+
+            </div>
+
+
+            <!-- Infos pequenas -->
+            <div class="row">
+
+                <div class="col-md-6">
+                    <label for="formGroupExampleInput">Carga horária</label>
+                    <input type="text" class="form-control" id="carga_Mcurso" name="carga_Mcurso" placeholder="">
+                </div>
+
+
+                <div class="col-md-6">
+                    <label for="formGroupExampleInput">Vagas</label>
+                    <input type="text" class="form-control" id="vagas_Mcurso" name="vagas_Mcurso" placeholder="">
+                </div>
+
+
+
+            </div>
+
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
+            <button type="submit" class="btn btn-primary">Salvar alterações</button>
+          </div>
+
+
+        </form>
+
+
+
+      </div>
+    </div>
+  </div>
+</div>
+
 
  <?php
 
@@ -120,116 +236,21 @@
 
 <?php
 
-    $curso = $sql->select("SELECT DISTINCT M.id_Mcurso, M.nome_Mcurso, M.des_Mcurso, M.local_Mcurso, M.dia_Mcurso,
+    $minicursos = $sql->select("SELECT DISTINCT M.id_Mcurso, M.nome_Mcurso, M.des_Mcurso, M.local_Mcurso, M.dia_Mcurso,
                             M.ministrante_Mcurso, M.ch_Mcurso, M.vagas_Mcurso, M.img_Minicurso, M.FK_Curso_id_curso 
                             FROM Minicurso as M
-                            INNER JOIN Curso as C on M.FK_Curso_id_curso = C.id_curso 
+                            INsNER JOIN Curso as C on M.FK_Curso_id_curso = C.id_curso 
                             ORDER BY M.nome_Mcurso");
+
+
+    $cursos = $sql->select("SELECT id_curso, nome_curso, imgPadrao_curso FROM Curso");
+
 
 ?>
 
 
 
-<!-- Modal -->
-<div class="modal fade" data-backdrop="static" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-  <div class="modal-dialog modal-dialog-centered" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalCenterTitle">Alterar dados</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-            
 
-        <form>
-
-            <!--  Escolha do Curso -->
-            <label for="exampleInputEmail1">Curso</label>
-            <select class="form-control">
-                <option>Escolha o curso</option>
-            </select>
-
-
-            <!-- Nome -->
-            <div class="form-group">
-                <label for="formGroupExampleInput">Nome</label>
-                <input type="text" class="form-control" id="formGroupExampleInput" placeholder="">
-            </div>
-
-            <!-- Descrição -->
-            <div class="form-group">
-                <label for="exampleFormControlTextarea1">Descrição</label>
-                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-            </div>
-
-            <!-- Local -->
-            <div class="form-group">
-                <label for="formGroupExampleInput">Local</label>
-                <input type="text" class="form-control" id="formGroupExampleInput" placeholder="">
-            </div>
-
-
-            <!-- Imagen -->
-            <div class="form-group">
-                <label for="exampleFormControlFile1">Imagem</label>
-                <input type="file" class="form-control-file" id="exampleFormControlFile1">
-            </div>
-
-
-            <div class="row">
-                <div class="col-md-6">
-                    <!--  Escolha do dia -->
-                    <label for="exampleInputEmail1">Dia</label>
-                    <select class="form-control">
-                        <option>16</option>
-                        <option>17</option>
-                        <option>18</option>
-                        <option>19</option>
-                    </select>                    
-                </div>
-
-                 <div class="col-md-6">
-                    <label for="formGroupExampleInput">Hora</label>
-                    <input type="text" class="form-control" id="formGroupExampleInput" placeholder="">
-                </div>               
-
-            </div>
-
-
-            <!-- Infos pequenas -->
-            <div class="row">
-
-                <div class="col-md-6">
-                    <label for="formGroupExampleInput">Carga horária</label>
-                    <input type="text" class="form-control" id="formGroupExampleInput" placeholder="">
-                </div>
-
-
-                <div class="col-md-6">
-                    <label for="formGroupExampleInput">Vagas</label>
-                    <input type="text" class="form-control" id="formGroupExampleInput" placeholder="">
-                </div>
-
-
-
-            </div>
-
-
-        </form>
-
-
-
-
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
-        <button type="button" class="btn btn-primary">Salvar alterações</button>
-      </div>
-    </div>
-  </div>
-</div>
 
 
 
--- programacao-minucursos.php
+++ programacao-minucursos.php
@@ -65,7 +65,7 @@
 						<div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
 							<a class="dropdown-item" href="minicursos.html">Minicursos</a>
 							<a class="dropdown-item" href="palestras.html">Palestras</a>
-							<a class="dropdown-item" href="projetos.html">Projetos</a>
+							<a class="dropdown-item" href="mesaredonda.html">Mesa redonda</a>
 						</div>
 					</div>	
 				</li>
@@ -76,7 +76,7 @@
 						<div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
 							<a class="dropdown-item" href="#">Minicursos</a>
 							<a class="dropdown-item" href="programacao-palestras.html">Palestras</a>
-							<a class="dropdown-item" href="programacao-projetos.html">Projetos</a>
+							<a class="dropdown-item" href="programacao-mesaredonda.html">Mesa redonda</a>
 						</div>
 					</div>	
 				</li>
@@ -93,77 +93,14 @@
 <!-- FIM MENU -->
 
 
+
+
+
 		<div class="programaco">
 			
         <div class="container">
 
         	<div class="row prog">
-
-
-
-
-
-
-
-
-					<div class="row">
-
-
-<!--  Aqui começa a gerar os cursos  -->
-<?php 
-
-	require_once("DAO/config.php");
-	require_once("DAO/class/sql.php");
-
-	$sql = new Sql();
-
-	$cursos = $sql->select("SELECT id_curso, nome_curso, imgPadrao_curso FROM Curso");
-
-
-/* Esse for roda por todos os cursos */
-	foreach ($cursos as $row) {
-
-	$nomeCurso = array();
-	$contC = 0;
-
-/* Esse for pega as informações do curso e insere em nomeCurso */
-	$cont1 = 0;
-	foreach ($row as $key => $value) {
-		
-		$nomeCurso[$cont1] = $value;
-		$cont1++;
-
-	}
-
-?>
-
-
-
-
-
-<!----------------------------------------------- CURSO  1 ----------------------------------------------------------->
-
-					    <div class="col-md-12">
-
-						     <a class="btn-duv" href="#" data-toggle="collapse" data-target="#<?= $nomeCurso[0]?>" aria-expanded="true">
-									    <div class="row">
-									    	<div class="col-md-12 ">
-									    		 <span class="duv-text">
-									    		 	<?php
-									    		 	echo $nomeCurso[1];
-									    		 	?>
-
-									    		 </span>
-							                </div>
-							            </div>
-						    </a>
-
-						    <div id="<?= $nomeCurso[0]?>" class="collapse" aria-labelledby="headingOne" data-parent="#accordion">
-
-						     	<div class="card-body resposta">
-
-						        	<div class="row row-card">
-
 
         		<div class="col-md-2">
 
@@ -178,56 +115,9 @@
         		</div>
 
 
-<?php
 
 
-/*************************************************************
-
- AQUI NASCEM OS MINICURSOS
-
-*************************************************************/
-
-$minicursos = $sql->select("SELECT * FROM Minicurso INNER JOIN Curso on Minicurso.FK_Curso_id_curso = Curso.id_curso WHERE FK_Curso_id_curso = $nomeCurso[0]");
-
-
-foreach ($minicursos as $row) {
-
-	$valor = array();
-
-
-/* Esse for pegas as informações dos minicursos e joga em valor */
-	$cont = 0;
-	foreach ($row as $key => $value) {
-		
-		$valor[$cont] = $value;
-		$cont++;
-
-	}
-
-/* Trata a imagem do minicurso */
-
-$imgMinicurso;
-
-if (file_exists($valor[8])){
-	$imgMinicurso = $valor[8];
-} else {
-	$imgMinicurso = $nomeCurso[2];
-}
-
-/* Trata a cor dos card */
-
-if (($contC % 2) == 0){
-	$corCard = "card-azul-claro";
-} else {
-	$corCard = "card-azul-escuro";
-}
-		?>
-
-										<div class="col-md-4 card-full">
-
-											  <a data-toggle="modal" data-target="#<?= $nomeCurso[0]; ?>a<?=$contC; ?>">
-
-												        		<div class="tab-content" id="myTabContent">
+        		<div class="tab-content" id="myTabContent">
 
 
         			<!--------------------------- DIA 1 ------------------------------->
@@ -250,11 +140,14 @@ if (($contC % 2) == 0){
 										    <h5 class="card-title">Local: Sala 1</h5>
 										  </div>
 										</div>
-
+										<div class="card text-white bg-info mb-3" style="width: 100%;">
+										  <div class="card-header">Big Data: Introdução</div>
+										  <div class="card-body">
+										    <h5 class="card-title">Local: Sala 5</h5>
+										  </div>
+										</div>	
 		        				</div>
 		        			</div>
-
-		        			<!-- HORA -->
 
 		        		</div> 						
 
@@ -282,7 +175,19 @@ if (($contC % 2) == 0){
 									  <div class="card-body">
 									    <h5 class="card-title">Local: Sala 3</h5>
 									  </div>
+									</div>
+									<div class="card text-white bg-info mb-3" style="width: 100%;">
+									  <div class="card-header">Binario passo a passo</div>
+									  <div class="card-body">
+									    <h5 class="card-title">Local: Sala 20</h5>
+									  </div>
 									</div>	
+									<div class="card text-white bg-info mb-3" style="width: 100%;">
+									  <div class="card-header">Linux para iniciantes</div>
+									  <div class="card-body">
+									    <h5 class="card-title">Local: Sala 17</h5>
+									  </div>
+									</div>		
 		        				</div>
 		        			</div>
 
@@ -300,24 +205,38 @@ if (($contC % 2) == 0){
 		        		<div class="col-md-10 dia-">
 		        			<span class="title">Minicursos</span>
 
+		        			<!-- HORA -->
+
+
 		        			<div class="row ofertas">
 		        				<div class="col-2 horario">
-		        					<span class="text-hora">8:00<br></span>
+		        					<span class="text-hora">10:00<br></span>
 		        					
 		        					<img class="linha" src="img/linha.png">
 		        				</div>
 
 		        				<div class="col-10">
-		        					<div class="card text-white bg-info mb-3" style="width: 100%;">
-									  <div class="card-header">HTML e CSS para iniciantes</div>
+									<div class="card text-white bg-info mb-3" style="width: 100%;">
+									  <div class="card-header">PHP é legal</div>
 									  <div class="card-body">
-									    <h5 class="card-title">Local: Sala 3</h5>
+									    <h5 class="card-title">Local: Sala 11</h5>
+									  </div>
+									</div>
+									<div class="card text-white bg-info mb-3" style="width: 100%;">
+									  <div class="card-header">Internet das coisas</div>
+									  <div class="card-body">
+									    <h5 class="card-title">Local: Sala 31</h5>
+									  </div>
+									</div>
+									<div class="card text-white bg-info mb-3" style="width: 100%;">
+									  <div class="card-header">Gamificação</div>
+									  <div class="card-body">
+									    <h5 class="card-title">Local: Sala 10</h5>
 									  </div>
 									</div>	
 		        				</div>
 		        			</div>
-
-		        			<!-- HORA -->
+		        			<!-- FIM HORA -->
 
 		        		</div>
 
@@ -328,104 +247,12 @@ if (($contC % 2) == 0){
         		</div>
 
 
-											  </a>	
-
-											<!-- Modal -->
-
-											<div class="modal fade" id="<?= $nomeCurso[0]; ?>a<?=$contC; ?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-											    <div class="modal-dialog modal-dialog-centered" role="document">
-											    	<div class="modal-content">
-
-												        <div class="modal-header">
-													        <h5 class="modal-title" id="exampleModalCenterTitle">
-													        	
-													        <?php
-													   		echo $valor[1];
-													   		?>
-
-													        </h5>
-													        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-													        <span aria-hidden="true">&times;</span>
-													        </button>
-												     	</div>
-
-												    	<div class="modal-body">
-												        <?php
-													   		echo $valor[2];
-													   	?>
-												        .<br><br>
-												        <span class="sobre">
-												        Ministrante: 
-												        <?php
-													   		echo $valor[5];
-													   	?>	
-												        <br>
-												        Carga horária: 
-									  		    		<?php
-													   		echo $valor[6];
-													   	?>	
-									  		    		<br>
-									  		    		Número de vagas:
-											    		<?php
-													   		echo $valor[7];
-													   	?>	<br>
-													   	Local: 
-											    		<?php
-													   		echo $valor[3];
-													   	?>	
-											    		<br>
-											    		<?php
-														  	$date = new DateTime($valor[4]);
-															echo $date->format('d/m/Y');
-															echo " às ".$date->format('H')."h".$date->format('m')."min";
-														 ?>
-											    		</span>
-											    		</div>
-
-												     	<div class="modal-footer">
-												        <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
-												     	</div>
-
-											    	</div>
-
-											    </div>
-
-											</div> 
-
-						        		</div>
-
-<?php
+        	</div>    
 
 
-//Esse cont é para os id de minicursos
-$contC++;
-
-//Fechamento do for de cada minicurso
-}
-
-/************************************************************
-
- AQUI JÁS OS MINICURSOS
-
-*************************************************************/
-
-?>
-	        		</div>
-				</div>
-	       	</div>
-	    </div>
-
-<?php
-
-//Fechamento do for de cada curso
-}
-
-?>
-
-					</div>
 
 
-        	</div>   
+
 
 
         </div>    
